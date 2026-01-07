fd4de41b4f3b785a1226361abd5f8b8a6cdf0330
lyclyq <llylcq@163.com>
2026-01-07 15:20:05 +1100

    Delete optimization directory

--- optimization/utils/seed.py
+++
@@ -1,5 +0,0 @@
-# utils/seed.py
-import random, numpy as np, torch
-def set_seed(s: int):
-    random.seed(s); np.random.seed(s); torch.manual_seed(s)
-    if torch.cuda.is_available(): torch.cuda.manual_seed_all(s)
